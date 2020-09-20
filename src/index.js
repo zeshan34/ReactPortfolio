bde56e8754352b9e26224ac15bbbbb2f63cfce15
zeshan <shaikhzeshan@gmail.com>
2020-09-19 23:04:00 -0500

    counter component badge and btn classess

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Counter from './components/counterComponent';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
